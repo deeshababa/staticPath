411360ff323272e2b4ebf61aa13d9deb0d56e9f9
deeshababa <105352872+deeshababa@users.noreply.github.com>
2023-02-07 14:35:08 +0530

    removed the zipfolder

--- zipgetstaticProps/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    trailingSlash:true,
-    basePath:'/staticPath'
-  }
-  
-  module.exports = nextConfig
-  
